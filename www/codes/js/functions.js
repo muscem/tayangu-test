718abb5b9c59d245b04df19dd6ce54bd9164799e
muscem <muscem1@hotmail.com>
2017-01-31 01:48:48 +0200

    test
    test

--- www/codes/js/functions.js
+++ www/codes/js/functions.js
@@ -22,7 +22,7 @@ function onDeviceReady() {
 	// Now safe to use the PhoneGap API
 	//alert("Device is ready");
 	$.support.cors=true;
-	//$.mobile.allowCrossDomainPages = true;
+	$.mobile.allowCrossDomainPages = true;
     //$.mobile.pushStateEnabled = false;
 }
 
