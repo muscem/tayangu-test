30af0d3c91992f453f024ad389479aed7006f9c7
muscem <muscem1@hotmail.com>
2017-01-31 02:15:26 +0200

    test
    test

--- www/codes/js/functions.js
+++ www/codes/js/functions.js
@@ -39,8 +39,8 @@ function user_control(){
 		timeout: 260000,
 		data: {un:"mci",
 				p:"123456",
-				s:"user_login_control",
-				uuid:device.uuid
+				s:"user_login_control"
+				//uuid:device.uuid
 			},
 		dataType: "xml"
 	})
